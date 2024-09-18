bd3c866c087d1de0137c489cc8f46c384a9c56d9
EvgenKh <hamitcevich@gmail.com>
2024-09-18 23:28:00 +0300

    feat: finish style the player

--- audio-player/index.js
+++ audio-player/index.js
@@ -3,13 +3,27 @@ const audio = document.querySelector('.song');
 const playBtn = document.querySelector('.play-btn');
 const pauseBtn = document.querySelector('.pause-btn');
 
+let isPlay = false;
+
 function playAudio() {
+  if (!isPlay){
+    // audio.src = 'assets/audio/dontstartnow.mp3';
   audio.currentTime = 0;
   audio.play();
+  isPlay = true;
+  } else {
+
+  }
 }
 
 function pauseAudio() {
   audio.pause();
+  isPlay = false;
+}
+
+function playOrStop() {
+
+
 }
 
 playBtn.addEventListener('click', playAudio);
